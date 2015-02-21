08dd62a424aa7bd5b6cb7aa780477d2640941a93
Carlo Colombo <carlo.colombo@gmx.com>
2015-02-21 19:26:04 +0100

    using data_dir from configuration

--- src/main/java/mtp/MtpConfiguration.java
+++ src/main/java/mtp/MtpConfiguration.java
@@ -9,16 +9,19 @@ import mtp.services.DatastoreService;
 import mtp.services.DatastoreServiceImpl;
 import net.openhft.chronicle.map.ChronicleMapBuilder;
 
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Bean;
 
 @org.springframework.context.annotation.Configuration
 public class MtpConfiguration {
 
+	private static final String DATA_DIR = StringUtils.defaultIfBlank(
+			System.getenv("OPENSHIFT_DATA_DIR"), "/tmp");
+
 	public ConcurrentMap<String, Entry> datastore() {
 		try {
 			return ChronicleMapBuilder.of(String.class, Entry.class)
-					.createPersistedTo(new File("/tmp/mtp"));
+					.createPersistedTo(new File(DATA_DIR + "/datastore"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
